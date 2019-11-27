66f51c91a6141f96dffec710994077a193406e4a
KunMinX <kunminx@gmail.com>
2019-11-27 14:26:16 +0800

    补充遗漏文件

---
+++ app/src/main/java/com/kunminx/puremusic/ui/helper/DrawerCoordinateHelper.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2018-2019 KunMinX
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kunminx.puremusic.ui.helper;
+
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.kunminx.architecture.bridge.callback.UnPeekLiveData;
+import com.kunminx.puremusic.bridge.callback.SharedViewModel;
+import com.kunminx.puremusic.ui.base.BaseFragment;
+
+/**
+ * TODO tip：通过 Lifecycle 来解决抽屉侧滑禁用与否的判断的 一致性问题，
+ * <p>
+ * 每个需要注册和监听生命周期来判断的视图控制器，无需在各自内部手动书写解绑等操作。
+ * 如果这样说还不理解，详见 https://xiaozhuanlan.com/topic/3684721950
+ * <p>
+ * Create by KunMinX at 19/11/3
+ */
+public class DrawerCoordinateHelper implements DefaultLifecycleObserver, View.OnTouchListener {
+
+    private float downX;
+    private float downY;
+
+    public final UnPeekLiveData<Boolean> openDrawer = new UnPeekLiveData<>();
+
+    private static DrawerCoordinateHelper sHelper = new DrawerCoordinateHelper();
+
+    public static DrawerCoordinateHelper getInstance() {
+        return sHelper;
+    }
+
+    private DrawerCoordinateHelper() {
+    }
+
+    @Override
+    public void onCreate(@NonNull LifecycleOwner owner) {
+
+        SharedViewModel.tagOfSecondaryPages.add(owner.getClass().getSimpleName());
+
+        ((BaseFragment) owner).getSharedViewModel()
+                .enableSwipeDrawer.setValue(SharedViewModel.tagOfSecondaryPages.size() == 0);
+    }
+
+    @Override
+    public void onDestroy(@NonNull LifecycleOwner owner) {
+
+        SharedViewModel.tagOfSecondaryPages.remove(owner.getClass().getSimpleName());
+
+        ((BaseFragment) owner).getSharedViewModel()
+                .enableSwipeDrawer.setValue(SharedViewModel.tagOfSecondaryPages.size() == 0);
+    }
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                downX = x;
+                downY = y;
+                break;
+            case MotionEvent.ACTION_UP:
+                float dx = x - downX;
+                float dy = y - downY;
+                if (Math.abs(dx) > 8 && Math.abs(dy) > 8) {
+                    int orientation = getOrientation(dx, dy);
+                    switch (orientation) {
+                        case 'r':
+                            openDrawer.setValue(true);
+                            break;
+                        case 'l':
+                            break;
+                        case 't':
+                            break;
+                        case 'b':
+                            break;
+                    }
+                }
+                break;
+        }
+        return false;
+    }
+
+    private int getOrientation(float dx, float dy) {
+        if (Math.abs(dx) > Math.abs(dy)) {
+            return dx > 0 ? 'r' : 'l';
+        } else {
+            return dy > 0 ? 'b' : 't';
+        }
+    }
+}
